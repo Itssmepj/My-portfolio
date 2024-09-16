b3217e1458112fb8dd731644acfa425cdb8d16a4
Itssmepj <lakherapranjal@gmail.com>
2024-09-16 11:30:17 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    const profilePic = document.getElementById("profile");
+  
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  
+    if (menu.classList.contains("open")) {
+      profilePic.style.marginTop = "170px";
+    } else {
+      profilePic.style.marginTop = "0";
+    }
+=======
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    const profilePic = document.getElementById("profile");
+  
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  
+    if (menu.classList.contains("open")) {
+      profilePic.style.marginTop = "170px";
+    } else {
+      profilePic.style.marginTop = "0";
+    }
+>>>>>>> dac0e89 (Initial commit)
+  }
