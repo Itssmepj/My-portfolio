31e73cedf863977c12e9d7d2d9bfbe1281b66769
Itssmepj <lakherapranjal@gmail.com>
2024-09-16 11:44:14 +0530

    files updates

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -12,19 +11,4 @@ function toggleMenu() {
     } else {
       profilePic.style.marginTop = "0";
     }
-=======
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    const profilePic = document.getElementById("profile");
-  
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  
-    if (menu.classList.contains("open")) {
-      profilePic.style.marginTop = "170px";
-    } else {
-      profilePic.style.marginTop = "0";
-    }
->>>>>>> dac0e89 (Initial commit)
   }
